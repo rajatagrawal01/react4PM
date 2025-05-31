daada6d58efbdd572f5d85fd3c260e87bd59cfbd
rajatagrawal01 <rajatagrawal01@gmail.com>
2025-05-31 13:51:32 +0530

    fetchAPI

--- src/components/apiCall.js
+++ src/components/apiCall.js
@@ -2,13 +2,25 @@ import React, { useEffect, useState } from "react";
 
 function UserList() {
   const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((res) => res.json())
-      .then((data) => setUsers(data))
+      .then((data) => {
+        setUsers(data);
+        setLoading(false);
+      })
       .catch((err) => console.log(err));
   }, []);
+
+  if (loading)
+    return (
+      <>
+        <div class="spinner-grow text-primary" role="status"></div>
+        <span class="sr-only">Loading...</span>
+      </>
+    );
 
   return (
     <div>
